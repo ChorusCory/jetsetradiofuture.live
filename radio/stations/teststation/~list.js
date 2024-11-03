2c1ef3ac97a0cb73cbc6bcbacbf1186575878c05
ChorusCory <91793020+ChorusCory@users.noreply.github.com>
2024-11-03 13:10:17 -0500

    teststation

---
+++ radio/stations/teststation/~list.js
@@ -0,0 +1,13 @@
+//Choose a name for the station
+stationName = "teststation";
+
+//Add it to the array of stations
+stationsArray[stationsArray.length] = stationName;
+
+//Define an array for tracks
+this[stationName+"_tracks"] = new Array();
+
+
+//TRACKS
+
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Ludwig von Beethoven - Moonlight Sonata (Third Movement)";
